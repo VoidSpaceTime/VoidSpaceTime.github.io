3159afa8d1827c9a903bc4cb057ecb29f98ac026
时空 <Space_Time_@outlook.com>
2023-11-16 02:10:07 +0800

    updata

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -5,18 +5,18 @@ import theme from "./theme.js";
 
 
 export default defineUserConfig({
-  base: "/",
+  base: "/zh/",
   theme,
   locales: {
-    "/zh/": {
-      lang: "zh-CN",
-      title: "博客演示",
-      description: "vuepress-theme-hope 的博客演示",
-    },
     "/": {
       lang: "en-US",
       title: "Blog Demo",
       description: "A blog demo for vuepress-theme-hope",
+    },
+    "/zh/": {
+      lang: "zh-CN",
+      title: "博客演示",
+      description: "vuepress-theme-hope 的博客演示",
     },
   },
 
